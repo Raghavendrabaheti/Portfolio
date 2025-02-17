facfb6a7af2ef6ab2fb4463430045e4cd03c5d98
Raghavendrabaheti <bahetiraghav990@gmail.com>
2025-02-17 14:00:24 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,25 +1,30 @@
 import { motion } from 'framer-motion';
+import img from '../assets/img.webp';
 
 const Home = () => {
   return (
-    <section id="home" className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white px-6">
+    <section 
+      id="home" 
+      className="flex flex-col items-start justify-center h-screen text-white px-6"
+      style={{ backgroundImage: `url(${img})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
       <motion.div 
-        className="text-center max-w-3xl"
+        className="text-left max-w-3xl bg-opacity-50 p-8 rounded-lg"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <h1 className="text-4xl md:text-6xl font-bold mb-4">
-          Hi, I'm <span className="text-blue-500">Raghavendra Baheti</span>
+        <h1 className="text-4xl md:text-6xl text-[#beb6d2] font-bold mb-4">
+          Hi, I'm <span className="text-[#fa1e4e]">Raghavendra Baheti</span>
         </h1>
-        <p className="text-lg md:text-xl text-gray-300 mb-6">
+        <p className="text-lg md:text-xl text-[#beb6d2] mb-6">
           A passionate Java Developer focused on building interactive and user-friendly web applications.
         </p>
-        <div className="flex gap-4 justify-center">
-          <a href="#work" className="px-6 py-3 rounded-lg bg-blue-500 hover:bg-blue-600 transition">
+        <div className="flex gap-4">
+          <a href="#work" className="px-6 py-3 rounded-lg bg-[#fa1e4e] hover:bg-[#bf173c] text-black transition">
             View My Work
           </a>
-          <a href="#contact" className="px-6 py-3 rounded-lg border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white transition">
+          <a href="#contact" className="px-6 py-3 rounded-lg border border-[#fa1e4e] text-[#beb6d2] hover:bg-[#fa1e4e] hover:text-white transition">
             Contact Me
           </a>
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gray-900 shadow-md text-white py-4 px-6 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-[#131019] shadow-md text-white py-4 px-6 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-blue-500">Raghav</h1>
+    
         <ul className="flex space-x-6">
           <li>
             <Link to="/" className="cursor-pointer hover:text-blue-500 transition">
