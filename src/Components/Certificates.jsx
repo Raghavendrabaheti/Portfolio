72b24be4e8cc44b1e1bebcec39f9b2bcdaec1213
Raghavendrabaheti <bahetiraghav990@gmail.com>
2025-02-18 16:40:57 +0530

    updated

--- src/Components/Certificates.jsx
+++ src/Components/Certificates.jsx
@@ -5,7 +5,7 @@ const Certificates = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('https://api.github.com/repos/Raghavendrabaheti/Certificates/Components/certificates')
+    fetch('https://api.github.com/repos/Raghavendrabaheti/Certificates/contents/certificates.json')
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
