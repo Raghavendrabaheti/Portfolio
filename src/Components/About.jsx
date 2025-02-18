ae23739cbda8c276dbf9d9bd2f3c6acf9ca7964c
Raghavendrabaheti <bahetiraghav990@gmail.com>
2025-02-18 14:56:52 +0530

    updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,9 +2,9 @@ const About = () => {
   return (
     <section id="about" className="h-screen bg-[#8F9CD9] text-white flex flex-col items-center justify-center px-6">
       <h2 className="text-4xl font-bold text-blue-500 mb-4">About Me</h2>
-      <p className="text-lg text-gray-300 max-w-3xl text-center">
-        I'm a full-stack developer with experience in React, JavaScript, Node.js, and MongoDB. 
-        Passionate about building web applications that solve real-world problems.
+      <p className="text-lg  max-w-3xl text-center">
+      Hi Everyone, I’m Raghavendra Baheti  from Udaipur, India. I currently pursuing Computer Science and Engineering at Techno India NJR. I specialize in full-stack development, particularly in the MEARN (MongoDB, Express.js, React, Node.js) stack, along with DevOps. My expertise extends to backend development, cloud deployment, and automation, making me proficient in building scalable and secure applications.
+      With a strong foundation in Java and DSA, I focus on problem-solving, algorithm optimization, and competitive programming to enhance my coding skills.
       </p>
     </section>
   );
