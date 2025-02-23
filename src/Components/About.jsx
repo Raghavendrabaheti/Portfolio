807623b29303ce0346311057d261b09fc9dcc594
Raghavendrabaheti <bahetiraghav990@gmail.com>
2025-02-23 23:38:25 +0530

    adjust Certificates component padding for improved layout and spacing

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
       <p className="text-lg  max-w-3xl text-center">
       Hi Everyone, I’m Raghavendra Baheti  from Udaipur, India. I currently pursuing Computer Science and Engineering at Techno India NJR. I specialize in full-stack development, particularly in the MEARN (MongoDB, Express.js, React, Node.js) stack, along with DevOps. My expertise extends to backend development, cloud deployment, and automation, making me proficient in building scalable and secure applications.
       With a strong foundation in Java and DSA, I focus on problem-solving, algorithm optimization, and competitive programming to enhance my coding skills.
+      
       </p>
     <div className="flex justify-center items-center h-screen">
       <GitHubCalendar username="Raghavendrabaheti" />
